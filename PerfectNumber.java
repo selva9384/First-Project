0c9cd0d6b29b30a0e093ce43647a550c586cd278
selva9384 <selvendiran938496@gmail.com>
2022-03-22 13:57:10 +0530

    Updated

---
+++ PerfectNumber.java
@@ -0,0 +1,33 @@
+package tamilNadu.chennai;
+
+import java.util.Scanner;
+
+public class PerfectNumber {
+
+	public static void main(String[] args) {
+		PerfectNumber obj=new PerfectNumber();
+		obj.perfactNumber();
+	}
+
+	private void perfactNumber() {
+		System.out.println("Enter a number");
+		Scanner sc=new Scanner(System.in);
+		int number=sc.nextInt(),i=1,sum=0;
+		sc.close();
+		while(i<number)
+		{
+			if((number%i)==0)
+			{
+				sum+=i;
+			}
+			i++;
+		}
+		if(number==sum) {
+			System.out.println(number+" is a perfect number");
+		}
+		else {
+			System.out.println(number+" is not a perfect number");
+		}
+	}
+
+}
