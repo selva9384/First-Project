601cf8cbb520094449e7f2a3d8293de9d19a3379
selva <selva@gamil.com>
2022-03-28 21:37:55 +0530

    polindrome

---
+++ PolindromeInString.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class PolindromeInString {
+
+	public static void main(String[] args) {
+		PolindromeInString o=new PolindromeInString();
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a word");
+		o.polindrome(sc.next());
+	}
+
+	private void polindrome(String word) {
+		int i=word.length()-1;
+		String compare="";
+		while(i>=0)
+		{
+			compare=word.charAt(i)+compare;
+			i--;
+		}
+		System.out.println(compare);
+		if(word.equals(compare))
+			System.out.println(word+" is a polindrome");
+		else
+			System.out.println(word+" is not a polindrome");
+	}
+
+}
