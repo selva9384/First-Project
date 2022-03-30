20688a332bdd3ba17d72c846b8219180868766d8
selva9384 <selvendiran938496@gmail.com>
2022-03-30 20:46:37 +0530

    Squre

---
+++ Pattern_programs/Square.java
@@ -0,0 +1,19 @@
+public class Square
+{
+	public static void main(String[] args)
+	{
+		Square sq=new Square();
+		sq.square();
+	}
+	public void square()
+	{
+		for(int i=1;i<=5;i++)
+		{
+			for(int j=1;j<=5;j++)
+			{
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+}
