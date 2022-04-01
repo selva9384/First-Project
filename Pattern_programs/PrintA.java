89fc9bc870cbef688a861c587dd0cbe5815f9932
selva9384 <selvendiran938496@gmail.com>
2022-04-01 23:34:33 +0530

    Print A with *

---
+++ Pattern_programs/PrintA.java
@@ -0,0 +1,34 @@
+
+public class PrintA {
+
+	public static void main(String[] args) {
+		PrintA A=new PrintA();
+		A.print();
+	}
+
+	private void print() {
+		for(int row=1;row<=6;row++)
+		{
+			for(int j=row;j<=6;j++)
+			{
+				System.out.print(" ");
+			}
+			
+			for(int i=1;i<=1;i++)
+			{
+				System.out.print("*");
+			}
+			for(int j=1;j<row;j++)
+			{
+				if(row==4) System.out.print("**");
+				else System.out.print("  ");
+			}
+			for(int i=1;i<=1;i++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
