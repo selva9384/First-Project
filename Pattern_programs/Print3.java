ab7aecbea3220bf3c46fb581d8bbf6d3c5c0a432
selva9384 <selvendiran938496@gmail.com>
2022-04-01 22:59:48 +0530

    Print 3 with *

---
+++ Pattern_programs/Print3.java
@@ -0,0 +1,31 @@
+public class Print3 {
+
+	public static void main(String[] args) {
+		Print3 p3=new Print3();
+		p3.print();
+	}
+
+	private void print() {
+		for(int row=1;row<10;row++)
+		{
+			if(row==1||row==9||row==5)
+			{
+				for(int i=1;i<=6;i++)
+				{
+					System.out.print(" *");
+				}
+			}
+			else
+			{
+				//System.out.print("*");
+				for(int i=1;i<=11;i++)
+				{
+					System.out.print(" ");
+				}
+				System.out.print(" *");
+			}
+			System.out.println();
+		}
+	}
+
+}
