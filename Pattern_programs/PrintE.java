1e38fdb03270b3372f6e551bb6bb6f3efb06cc91
selva9384 <selvendiran938496@gmail.com>
2022-04-01 22:45:39 +0530

    Print E with *

---
+++ Pattern_programs/PrintE.java
@@ -0,0 +1,26 @@
+public class PrintE {
+
+	public static void main(String[] args) {
+		PrintE B=new PrintE();
+		B.print();
+	}
+
+	private void print() {
+		for(int row=1;row<10;row++)
+		{
+			if(row==1||row==9||row==5)
+			{
+				for(int i=1;i<=7;i++)
+				{
+					System.out.print("* ");
+				}
+			}
+			else
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
