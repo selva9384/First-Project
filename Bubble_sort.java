95135a79ad41e9977ce13f0adc398dbec39d4d18
selva9384 <selvendiran938496@gmail.com>
2022-04-13 23:20:03 +0530

    Sort an integr array using bubble sort algorithm

--- Bubble_sort.java
+++ Bubble_sort.java
@@ -1,14 +1,15 @@
-
+//sort an integer array using bubble sort algorithm
 public class Bubble_sort {
 
 	public static void main(String[] args) {
-		sort();
+		int[] arr = { 4, 2, 5, 6, 7, 1 ,0,3};
+		sort_ascenting_order(arr);
+		sort_descenting_order(arr);
 	}
 
-	private static void sort() {
-		int[] arr = { 4, 2, 5, 6, 7, 1 ,0,3};
+	private static void sort_descenting_order(int[] arr) {
 		int temp = 0;
-		for(int j=1;j<arr.length;j++)
+		for(int j=1;j<=arr.length;j++)
 		{
 		for (int i = 0; i < arr.length - j; i++) {
 			if (arr[i] < arr[i + 1]) {
@@ -18,8 +19,27 @@ public class Bubble_sort {
 			}
 		}
 		}
+		System.out.println("Descenting");
 		for(int i:arr)
 			System.out.print(i+" ");
+	}
+
+	private static void sort_ascenting_order(int[] arr) {
+		int temp = 0;
+		for(int j=1;j<=arr.length;j++)
+		{
+		for (int i = 0; i < arr.length - j; i++) {
+			if (arr[i] > arr[i + 1]) {
+				temp = arr[i];
+				arr[i] = arr[i + 1];
+				arr[i + 1] = temp;
+			}
+		}
+		}
+		System.out.println("Ascending");
+		for(int i:arr)
+			System.out.print(i+" ");
+		System.out.println();
 	}
 
 }
