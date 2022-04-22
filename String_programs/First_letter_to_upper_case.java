4adfc0b2ae2b7a73a448874739e3367c60d964bd
selva9384 <selvendiran938496@gmail.com>
2022-04-22 21:37:35 +0530

    first letter to uppercase

---
+++ String_programs/First_letter_to_upper_case.java
@@ -0,0 +1,30 @@
+
+//program to convert lower case into upper case
+import java.util.Scanner;
+
+public class First_letter_to_upper_case {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a sentence");
+		String str = sc.nextLine();
+		sc.close();
+		System.out.println(toUpperCase(str));
+
+	}
+
+	private static String toUpperCase(String str) {
+		String upper = "";
+		for (int i = 0; i < str.length(); i++) {
+			if(i==0 || str.charAt(i-1)==' ')
+			{
+			if (str.charAt(i) >= 97 && str.charAt(i) <= 122)
+				upper += (char) (str.charAt(i) - 32);
+		}
+			else
+				upper += (char) (str.charAt(i));
+		}
+		return upper;
+	}
+
+}
